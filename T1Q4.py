c33dbda1de6ae067807d96d090d67504bf4b6a71
shirabod <154739064+shirabod@users.noreply.github.com>
2025-09-07 22:38:28 +0300

    T1

---
+++ T1Q4.py
@@ -0,0 +1,25 @@
+# Shira Choen 212079875
+# Shira Erel 213173636
+
+def prime(n):
+    if n <= 1:
+        return False
+    return list(filter(lambda x: n % x == 0, range(2, int(n**0.5) + 1))) == []
+
+def twin_primes(n):
+    if prime(n):
+        if prime(n+2):
+            return n+2
+        elif prime(n-2):
+            return n-2
+    
+    return "invalid input"
+
+def n_twin_primes(n):
+    twints = dict(map(lambda x: (x, twin_primes(x)), 
+                      filter(lambda x: twin_primes(x) != "invalid input", range(2, int(n)+1))))
+    return twints
+
+print("enter prime number:")
+u = input().strip()
+print(n_twin_primes(u))
